7323468d6528d9b4d5e297166b5498536e96f423
Jan Nypl <nyplj22@spstrutnov.cz>
2025-04-28 14:47:17 +0200

    no jako něco to dělá takže good i guess

---
+++ my-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://criywvntaywwaqpmopij.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaXl3dm50YXl3d2FxcG1vcGlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NDE5MzYsImV4cCI6MjA2MTQxNzkzNn0.PaG3XKxFd2TGZ8wTOo19iddC8vYV9UhiJrLlpSOe3sU");
+
+function App() {
+  const [instruments, setInstruments] = useState([]);
+
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    setInstruments(data);
+  }
+
+  return (
+    <ul>
+      {instruments.map((instrument) => (
+        <li key={instrument.name}>{instrument.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
