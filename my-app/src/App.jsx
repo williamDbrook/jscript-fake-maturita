367bec60c7401f1f09b97d374d1ed4a0d8b26d51
Jan Nypl <nyplj22@spstrutnov.cz>
2025-05-05 10:15:24 +0200

    Vite

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import SignUpSignIn from "./SignUpSignIn"; // Import the SignUpSignIn component
 
-const supabase = createClient("https://criywvntaywwaqpmopij.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaXl3dm50YXl3d2FxcG1vcGlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NDE5MzYsImV4cCI6MjA2MTQxNzkzNn0.PaG3XKxFd2TGZ8wTOo19iddC8vYV9UhiJrLlpSOe3sU");
+const supabase = createClient("https://criywvntaywwaqpmopij.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaXl3dm50YXl3d2FxcG1vcGlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTg0MTkzNiwiZXhwIjoyMDYxNDE3OTM2fQ.dxW9jfgOnF4BhFiE7pBLOFiXVYDn6U1qDVo6yjx1A0s");
 
 function App() {
   const [instruments, setInstruments] = useState([]);
